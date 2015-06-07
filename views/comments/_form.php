8c17352416afe278888448fc4989723800f24734
Skadelig <vindreise@gmail.com>
2015-06-08 04:29:45 +0600

    Создание заявки отредактировано

--- views/comments/_form.php
+++ views/comments/_form.php
@@ -3,6 +3,9 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\datetime\DateTimePicker;
+use yii\helpers\ArrayHelper;
+use app\models\Users;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Comments */
 /* @var $form yii\widgets\ActiveForm */
@@ -14,7 +17,10 @@ use kartik\datetime\DateTimePicker;
 
     <?= $form->field($model, 'Request_ID')->textInput() ?>
 
-    <?= $form->field($model, 'Users_id')->textInput() ?>
+    <?= $form->field($model, 'Users_id')->dropDownList(
+        ArrayHelper::map(Users::find()->all(),"Users_id",'Users_login')
+    ) ?>
+
 
     <!--<?= $form->field($model, 'Comment_date')->textInput() ?> -->
     <?= $form->field($model, 'Comment_date')->widget(DateTimePicker::classname(), 
